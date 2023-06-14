f9d796e577b633107995e07f17b2f51051f6234a
sfaludi <sfaludi@nomad-cms.com>
2023-06-14 11:24:56 -0700

    Fixed missing params

--- nomad-samples/js/asset-groups/asset-groups/add-asset-to-asset-group.js
+++ nomad-samples/js/asset-groups/asset-groups/add-asset-to-asset-group.js
@@ -1,19 +1,17 @@
 import * as prjConstants from "../constants/project-constants.js";
 import apiExceptionHandler from "../exceptions/api-exception-handler.js";
 
-export default async function addAssetToAssetGroup(authToken) 
+export default async function addAssetToAssetGroup(AUTH_TOKEN, ASSET_GROUP_ID, ASSETS)
 {
     // Create header for the request
     const HEADERS = new Headers();
     HEADERS.append("Content-Type", "application/json");
-    HEADERS.append("Authorization", `Bearer ${authToken}`);
+    HEADERS.append("Authorization", `Bearer ${AUTH_TOKEN}`);
   
-    const BODY = [
-        "9e92b187-745d-4c35-8265-088415dea57b"
-    ];
+    const BODY = ASSETS
   
   // Post
-    const RESPONSE = await fetch(`${prjConstants.PORTAL_API_URL}/assetgroup/add/{assetGroupId}`, {
+    const RESPONSE = await fetch(`${prjConstants.PORTAL_API_URL}/assetgroup/add/${ASSET_GROUP_ID}`, {
         method: "POST",
         headers: HEADERS,
         body: JSON.stringify(BODY)
--- nomad-samples/js/asset-groups/asset-groups/delete-asset-group.js
+++ nomad-samples/js/asset-groups/asset-groups/delete-asset-group.js
@@ -1,15 +1,15 @@
 import * as prjConstants from "../constants/project-constants.js";
 import apiExceptionHandler from "../exceptions/api-exception-handler.js";
 
-export default async function deleteAssetGroup(authToken) 
+export default async function deleteAssetGroup(AUTH_TOKEN, ASSET_GROUP_ID) 
 {
 		// Create header for the request
     const HEADERS = new Headers();
     HEADERS.append("Content-Type", "application/json");
-    HEADERS.append("Authorization", `Bearer ${authToken}`);
+    HEADERS.append("Authorization", `Bearer ${AUTH_TOKEN}`);
   
   // Post
-    const RESPONSE = await fetch(`${prjConstants.PORTAL_API_URL}/assetgroup/{assetGroupId}`, {
+    const RESPONSE = await fetch(`${prjConstants.PORTAL_API_URL}/assetgroup/${ASSET_GROUP_ID}`, {
         method: "DELETE",
         headers: HEADERS,
         body: JSON.stringify(BODY)
