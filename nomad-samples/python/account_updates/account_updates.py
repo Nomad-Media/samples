27da390c777124cda0a139908fa2196e54b35e97
sfaludi <sfaludi@nomad-cms.com>
2023-06-15 17:37:20 -0700

    Added user prompt

--- nomad-samples/python/account_updates/account_updates.py
+++ nomad-samples/python/account_updates/account_updates.py
@@ -4,24 +4,41 @@ from account.update_user import *
 
 import json
 
-def update_account(AUTH_TOKEN):
-    if not AUTH_TOKEN:
-        raise Exception("AUTH_TOKEN: Invalid authentication token")
+def update_user_main(AUTH_TOKEN):
+    try:
+        print("Updating user")
+        print(json.dumps(update_user(AUTH_TOKEN)))
+    except:
+        raise Exception()
 
+def change_email_main(AUTH_TOKEN):
     try:
         print("Changing email")
         change_email(AUTH_TOKEN)
+    except:
+        raise Exception()
 
+def change_password_main(AUTH_TOKEN):
+    try:
         print("Changing password")
         change_password(AUTH_TOKEN)
-    
-        print("Updating user")
-        print(json.dumps(update_user(AUTH_TOKEN)))
-        
     except:
         raise Exception("Failed to update account")
 
-
 if __name__ == "__main__":
-    AUTH_TOKEN = ""
-    update_account(AUTH_TOKEN)
+    AUTH_TOKEN = input("Enter your authentication token: ")
+    
+    while True:
+        print("Do you want to update the user, change your email or password, or quit")
+        USER_INPUT = input("Enter user for update user, email for change email, password for change password, and exit to quit: ")
+        
+        if USER_INPUT == "update user":
+            update_user_main(AUTH_TOKEN)
+        elif USER_INPUT == "change email":
+            change_email_main(AUTH_TOKEN)
+        elif USER_INPUT == "change password":
+            change_password_main(AUTH_TOKEN)
+        elif USER_INPUT == "exit":
+            break
+        else:
+            print("Input invalid")
