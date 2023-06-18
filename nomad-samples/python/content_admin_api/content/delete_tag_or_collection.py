ec5dba1cb2fe10ec54e3f43f3c3cee2778f17a21
sfaludi <sfaludi@nomad-cms.com>
2023-06-17 19:38:20 -0700

    Added cli input and renamed functions

---
+++ nomad-samples/python/content_admin_api/content/delete_tag_or_collection.py
@@ -0,0 +1,42 @@
+from constants.project_constants import *
+from exceptions.api_exception_handler import *
+
+import json, requests
+
+def delete_tag_or_collection(AUTH_TOKEN: str, TYPE: str, CONTENT_ID: str, TAG_ID: str, CONTENT_DEFINITION: str) -> dict:
+
+    # Check for valid parameters
+    if (not AUTH_TOKEN):
+        raise Exception("Authentication Token: The authentication token is invalid")
+
+    API_URL = ADMIN_API_URL + "/admin/" + TYPE + "/content/delete"
+        
+    # Create header for the request
+    HEADERS = {
+  	    "Authorization": "Bearer " + AUTH_TOKEN,
+        "Content-Type": "application/json"
+    }
+
+    # Build the payload BODY
+    BODY = {
+        "items": [
+            {
+                "contentDefinition": CONTENT_DEFINITION,
+                "contentId": CONTENT_ID,
+                "tagId": TAG_ID
+            }
+        ]
+    }
+
+    try:
+        # Send the request
+        RESPONSE = requests.post(API_URL, headers= HEADERS, data= json.dumps(BODY))
+
+        if RESPONSE.status_code != 200:
+            raise Exception("Response returned " + str(RESPONSE.status_code))
+
+        return json.loads(RESPONSE.text)
+
+    except:
+        api_exception_handler(RESPONSE, "delete tag or colleciton failed")
+
