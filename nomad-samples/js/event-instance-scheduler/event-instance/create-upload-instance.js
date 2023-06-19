9110ec532e6565ef79439b5d759c5aef9a1be93b
sfaludi <sfaludi@nomad-cms.com>
2023-06-19 13:10:33 -0700

    Created event instance scheduler code

--- nomad-samples/js/event-instance-scheduler/event-instance/create-upload-instance.js
+++ nomad-samples/js/event-instance-scheduler/event-instance/create-upload-instance.js
@@ -1,7 +1,11 @@
 import * as prjConstants from "../constants/project-constants.js";
 import apiExceptionHandler from "../exceptions/api-exception-handler.js";
 
-export default async function creatingAndUploadingEventInstance(AUTH_TOKEN, ID, CONTENT_ID, CONTENT_DEFINITION_ID, INSTANCE_NAME, START_DATETIME, END_DATETIME, DISABLED, DESCRIPTION, SLATE_VIDEO_ID, PREROLL_VIDEO_ID, POSTROLL_VIDEO_ID, IS_SECURE_OUTPUT, ARCHIVE_FOLDER, LIVE_INPUT_A_ID, LIVE_INPUT_B_ID, PRIMARY_LIVE_STREAM_URL, BACKUP_LIVESTREAM_URL) 
+export default async function creatingAndUploadingEventInstance(AUTH_TOKEN, ID, CONTENT_ID, 
+    CONTENT_DEFINITION_ID, INSTANCE_NAME, START_DATETIME, END_DATETIME, DISABLED, DESCRIPTION, 
+    SLATE_VIDEO_ID, PREROLL_VIDEO_ID, POSTROLL_VIDEO_ID, IS_SECURE_OUTPUT, ARCHIVE_FOLDER, 
+    LIVE_INPUT_A_ID, LIVE_INPUT_B_ID, PRIMARY_LIVE_STREAM_URL, BACKUP_LIVESTREAM_URL, 
+    SERIES_DESCRIPTION, SERIES_ID) 
 {
 		// Create header for the request
     const HEADERS = new Headers();
@@ -9,7 +13,6 @@ export default async function creatingAndUploadingEventInstance(AUTH_TOKEN, ID, 
     HEADERS.append("Authorization", `Bearer ${AUTH_TOKEN}`);
   
     const BODY = {
-        contentId: CONTENT_ID,
         contentDefinitionId: CONTENT_DEFINITION_ID,
 
         properties: {
@@ -25,18 +28,35 @@ export default async function creatingAndUploadingEventInstance(AUTH_TOKEN, ID, 
                 id: POSTROLL_VIDEO_ID
             },
             isSecureOutput: IS_SECURE_OUTPUT,
-            //liveInputA: {
-                //id: LIVE_INPUT_A_ID
-            //},
-            primaryLiveStreamInputUrl: PRIMARY_LIVE_STREAM_URL
+            primaryLiveStreamInputUrl: PRIMARY_LIVE_STREAM_URL,
+            series: {
+                properties: {}
+            }
+        },
+
+        series: {
+            properties: {}
         }
     };
 
-    
+    if (CONTENT_ID != "")
+    {
+        BODY.contentId = CONTENT_ID;
+    }
 
     if (ARCHIVE_FOLDER != "")
     {
         BODY.properties.archiveFolder = { id: ARCHIVE_FOLDER }; 
+    }
+
+    if (SLATE_VIDEO_ID != "")
+    {
+        BODY.properties.slateVideo = { id: SLATE_ID }
+    }
+
+    if (LIVE_INPUT_A_ID != "")
+    {
+        BODY.properties.liveInputA = { id: LIVE_INPUT_A_ID };
     }
 
     if (LIVE_INPUT_B_ID != "")
@@ -47,6 +67,17 @@ export default async function creatingAndUploadingEventInstance(AUTH_TOKEN, ID, 
     if (BACKUP_LIVESTREAM_URL != "")
     {
         BODY.properties.backupLiveStreamInputUrl = LIVE_INPUT_B_ID;
+    }
+
+    if (SERIES_DESCRIPTION != "")
+    {
+        //BODY
+        BODY.series.description = SERIES_DESCRIPTION;
+    }
+
+    if (SERIES_ID != "")
+    {
+        BODY.series.id = SERIES_ID;
     }
 
     // Post
