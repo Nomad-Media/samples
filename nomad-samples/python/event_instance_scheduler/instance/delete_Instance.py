ad8b78d6f7de7672a3627b4022f4e22197d88547
sfaludi <sfaludi@nomad-cms.com>
2023-06-19 20:38:28 -0700

    Added user input for parameters

--- nomad-samples/python/event_instance_scheduler/instance/delete_Instance.py
+++ nomad-samples/python/event_instance_scheduler/instance/delete_Instance.py
@@ -3,13 +3,13 @@ from exceptions.api_exception_handler import *
 
 import json, requests
 
-def deleting_event_instance(AUTH_TOKEN: str, CONTENT_ID: str, ID: str) -> dict:
+def deleting_event_instance(AUTH_TOKEN: str, CONTENT_ID: str, CONTENT_DEFINITION_ID: str) -> dict:
 
     # Check for valid parameters
     if (not AUTH_TOKEN):
         raise Exception("Authentication Token: The authentication token is invalid")
 
-    API_URL = ADMIN_API_URL + "/content/" + CONTENT_ID + "?contentDefinitionId=d34f116d-2a51-4d4a-b928-5dd581d9fd5e"
+    API_URL = f"{ADMIN_API_URL}/content/{CONTENT_ID}?contentDefinitionId={CONTENT_DEFINITION_ID}"
         
     # Create header for the request
     HEADERS = {
