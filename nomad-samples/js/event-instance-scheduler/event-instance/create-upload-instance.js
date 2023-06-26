6be5655bc1521f1e2a80c8abdd3f147dcb6473d0
sfaludi <sfaludi@nomad-cms.com>
2023-06-26 15:25:31 -0700

    Added series and recurring events

--- nomad-samples/js/event-instance-scheduler/event-instance/create-upload-instance.js
+++ nomad-samples/js/event-instance-scheduler/event-instance/create-upload-instance.js
@@ -2,58 +2,69 @@ import * as prjConstants from "../constants/project-constants.js";
 import apiExceptionHandler from "../exceptions/api-exception-handler.js";
 
 export default async function creatingAndUploadingEventInstance(AUTH_TOKEN, ID, CONTENT_ID, 
-    CONTENT_DEFINITION_ID, INSTANCE_NAME, START_DATETIME, END_DATETIME, DISABLED, DESCRIPTION, 
-    SLATE_VIDEO_ID, PREROLL_VIDEO_ID, POSTROLL_VIDEO_ID, IS_SECURE_OUTPUT, ARCHIVE_FOLDER, 
-    LIVE_INPUT_A_ID, LIVE_INPUT_B_ID, PRIMARY_LIVE_STREAM_URL, BACKUP_LIVESTREAM_URL, 
-    OVERRIDE_SERIES_DETAILS, SERIES_DESCRIPTION, SERIES_ID) 
+    CONTENT_DEFINITION_ID, INSTANCE_NAME, START_DATETIME, END_DATETIME, RECURRING_WEEKS, START_TIME, 
+    END_TIME, DATE_CHECKBOX, DISABLED, DESCRIPTION, SLATE_VIDEO_ID, PREROLL_VIDEO_ID, 
+    POSTROLL_VIDEO_ID, IS_SECURE_OUTPUT, ARCHIVE_FOLDER, LIVE_INPUT_A_ID, LIVE_INPUT_B_ID, 
+    PRIMARY_LIVE_STREAM_URL, BACKUP_LIVESTREAM_URL, OVERRIDE_SERIES_DETAILS, SERIES_DESCRIPTION, 
+    SERIES_ID, RECURRING, IS_CURRENT_SERIES) 
 {
-		// Create header for the request
+
+    // Create header for the request
     const HEADERS = new Headers();
     HEADERS.append("Content-Type", "application/json");
     HEADERS.append("Authorization", `Bearer ${AUTH_TOKEN}`);
-  
+
+    const DATE = DATE_CHECKBOX.startDate;
+
     const BODY = {
         contentDefinitionId: CONTENT_DEFINITION_ID,
 
         properties: {
             instanceName: INSTANCE_NAME,
-            startDatetime: START_DATETIME,
-            endDatetime: END_DATETIME,
+            startDatetime: RECURRING ? `${DATE.getFullYear()}-${DATE.getMonth() + 1}-${DATE.getDate()}T${START_TIME}` : START_DATETIME,
+            endDatetime: RECURRING ? `${DATE.getFullYear()}-${DATE.getMonth() + 1}-${DATE.getDate()}T${END_TIME}` : END_DATETIME,
             disabled: DISABLED,
-            description: DESCRIPTION,
             overrideSeriesDetails: OVERRIDE_SERIES_DETAILS,
-            isSecureOutput: IS_SECURE_OUTPUT,
-            primaryLiveStreamInputUrl: PRIMARY_LIVE_STREAM_URL,
-            backupLiveStreamInputUrl: BACKUP_LIVESTREAM_URL,
-            series: {
-                description: SERIES_DESCRIPTION,
-                id: SERIES_ID,
-                properties: {}
-            }
+            isRecurring: RECURRING,
         },
-
-        series: {
-            properties: {}
-        }
     };
 
-    PREROLL_VIDEO_ID != "" ? BODY.properties.prerollVideo = { id: PREROLL_VIDEO_ID } : BODY.properties.prerollVideo = "";
-
-    POSTROLL_VIDEO_ID != "" ? BODY.properties.postrollVideo = { id: POSTROLL_VIDEO_ID } : BODY.properties.postrollVideo = "";
-    
-    if (CONTENT_ID != "")
-    { 
-        BODY.contentId = CONTENT_ID 
+    if (RECURRING)
+    {
+        BODY.recurringDays = DATE_CHECKBOX.dates;
+        BODY.recurringWeeks = RECURRING_WEEKS;
     }
 
-    ARCHIVE_FOLDER != "" ? BODY.properties.archiveFolder = { id: ARCHIVE_FOLDER } : BODY.properties.archiveFolder = "";
+    if (IS_CURRENT_SERIES)
+    {
+        BODY.series = {
+            description: SERIES_DESCRIPTION,
+            id: SERIES_ID,
+            properties: {}
+        };
+    }
 
-    SLATE_VIDEO_ID != "" ? BODY.properties.slateVideo = { id: SLATE_ID } : BODY.properties.slateVideo = "";
+    if (OVERRIDE_SERIES_DETAILS || !IS_CURRENT_SERIES)
+    {
+        console.log(`Override : ${OVERRIDE_SERIES_DETAILS} Series: ${IS_CURRENT_SERIES}`);
+        BODY.description = DESCRIPTION;
+        BODY.isSecureOutput = IS_SECURE_OUTPUT;
+        BODY.primaryLiveStreamInputUrl = PRIMARY_LIVE_STREAM_URL;
+        BODY.backupLiveStreamInputUrl = BACKUP_LIVESTREAM_URL;
+        BODY.properties = {
+            prerollVideo: PREROLL_VIDEO_ID === "" ? "" : { id: PREROLL_VIDEO_ID },
+            postrollVideo: POSTROLL_VIDEO_ID === "" ? "" : { id: POSTROLL_VIDEO_ID },
+            archiveFolder: ARCHIVE_FOLDER === "" ? "" : { id: ARCHIVE_FOLDER },
+            slateVideo: SLATE_VIDEO_ID === "" ? "" : { id: SLATE_VIDEO_ID },
+            liveInputA: LIVE_INPUT_A_ID === "" ? "" : { id: LIVE_INPUT_A_ID },
+            liveInputB: LIVE_INPUT_B_ID === "" ? "" : { id: LIVE_INPUT_B_ID },
+        };
+    }
 
-    LIVE_INPUT_A_ID != "" ? BODY.properties.liveInputA = { id: LIVE_INPUT_A_ID } : BODY.properties.liveInputA = "";
-
-    LIVE_INPUT_B_ID != "" ? BODY.properties.liveInputB = { id: LIVE_INPUT_B_ID } : BODY.properties.liveInputB = "";
-
+    if (CONTENT_ID != "")
+    { 
+        BODY.contentId = CONTENT_ID;
+    }
     // Post
     const RESPONSE = await fetch(`${prjConstants.ADMIN_API_URL}/content/${ID}`, {
         method: "POST",
