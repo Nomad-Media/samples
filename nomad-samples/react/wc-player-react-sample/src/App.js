5adf159e837c96fd455bd6214f93ffc85bd4ea7f
Ilias Iliadis <il85@programmer.net>
2023-07-04 20:51:01 +0300

    Update the App.js with a way to consume wc-player events

--- nomad-samples/react/wc-player-react-sample/src/App.js
+++ nomad-samples/react/wc-player-react-sample/src/App.js
@@ -1,32 +1,50 @@
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+import React, { Component } from 'react';
+class App extends Component {
+  componentDidMount() {
+    this.component.addEventListener('playerEventsChanges', this.onPlayerEventChanges);
+  }
+
+  componentWillUnmount() {
+    this.component.addEventListener('playerEventsChanges', this.onPlayerEventChanges);
+  }
+
+  onPlayerEventChanges(event) {
+    // Handle the wc-player events here
+    console.log('React::playerEventsChanges', event);
+  }
+
+  handleRef = component => {
+    this.component = component;
+  };
+
+  render() {
+  return <div className="App">
       <wc-player
+          ref={this.handleRef}
           vep-config='{
-              "application": "Embedded",
-              "customer": "acme-customer",
-              "siteName": "Acme Main Site",
-              "hideSidebar": true,
-              "googleTagManager": "GTM-TAGGOESHERE",
-              "players": [{ "format": "hls", "player": "videojs" } ],
-              "liveMode": false,
-              "vepModules": [
-                {
-                  "moduleType": "streams",
-                  "isEnabled": true,
-                  "streams": [
-                    {
-                      "streamUrl": "stream URL goes here",
-                      "autoplay": true
-                    }
-                  ]
-                }
-              ]
+            "application": "Embedded",
+            "customer": "acme-customer",
+            "siteName": "Acme Main Site",
+            "hideSidebar": true,
+            "googleTagManager": "GTM-TAGGOESHERE",
+            "players": [{ "format": "hls", "player": "videojs" } ],
+            "liveMode": false,
+            "vepModules": [
+              {
+                "moduleType": "streams",
+                "isEnabled": true,
+                "streams": [
+                  {
+                    "streamUrl": "stream URL goes here",
+                    "autoplay": true
+                  }
+                ]
+              }
+            ]
           }'></wc-player>
     </div>
-  );
+    }
 }
-
 export default App;
