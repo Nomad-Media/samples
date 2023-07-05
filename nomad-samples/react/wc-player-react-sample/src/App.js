628692691af04090336a33e78be72e65f8a0ec37
Ilias Iliadis <il85@programmer.net>
2023-07-05 23:13:49 +0300

    Enhance wc-player to emit playback finished event for bitmovin and videojs

--- nomad-samples/react/wc-player-react-sample/src/App.js
+++ nomad-samples/react/wc-player-react-sample/src/App.js
@@ -25,24 +25,26 @@ class App extends Component {
           ref={this.handleRef}
           vep-config='{
             "application": "Embedded",
-            "customer": "acme-customer",
-            "siteName": "Acme Main Site",
-            "hideSidebar": true,
-            "googleTagManager": "GTM-TAGGOESHERE",
-            "players": [{ "format": "hls", "player": "videojs" } ],
-            "liveMode": false,
-            "vepModules": [
-              {
-                "moduleType": "streams",
-                "isEnabled": true,
-                "streams": [
-                  {
-                    "streamUrl": "stream URL goes here",
-                    "autoplay": true
-                  }
-                ]
-              }
-            ]
+              "customer": "dev-05",
+              "siteName": "Dev-05",
+              "hideSidebar": true,
+              "googleTagManager": "GTM-NFWDGJB",
+              "players": [{ "format": "progressive", "player": "videojs" } ],
+              "applicationId": "fc53821f-43a4-4758-9a31-87e3c66883ef",
+              "bitmovinLicenseKey": "f507de7a-720a-4b61-9698-94dc4a014479",
+              "liveMode": false,
+              "vepModules": [
+                {
+                  "moduleType": "streams",
+                  "isEnabled": true,
+                  "streams": [
+                    {
+                      "streamUrl": "https://content.dev-05.demos.media/Content/Public/the-joker.mp4",
+                      "autoplay": true
+                    }
+                  ]
+                }
+              ]
           }'></wc-player>
     </div>
     }
