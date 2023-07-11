5daefe9793cdc438b67fb5f61d4b243ddb1331aa
sfaludi <sfaludi@nomad-cms.com>
2023-07-11 16:20:18 -0700

    Movie walkthrough code

---
+++ nomad-samples/js/movies/create-movie/helpers/slugify.js
@@ -0,0 +1,12 @@
+export default function slugify(text) {
+    if (!text || text.trim().length == 0) return "";
+    return text
+        .toString()
+        .normalize("NFD")
+        .replace(/[\u0300-\u036f]/g, "")
+        .toLowerCase()
+        .trim()
+        .replace(/\s+/g, "-")
+        .replace(/[^\w-]+/g, "")
+        .replace(/--+/g, "-");
+}
