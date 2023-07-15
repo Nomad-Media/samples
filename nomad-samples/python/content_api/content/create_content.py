8a456b2a332e0a617bb2ded2fc9e3661bb8edc99
sfaludi <sfaludi@nomad-cms.com>
2023-07-14 17:14:53 -0700

    fixed string

---
+++ nomad-samples/python/content_api/content/create_content.py
@@ -0,0 +1,27 @@
+from constants.project_constants import *
+from exceptions.api_exception_handler import *
+
+import json, requests
+
+def create_content(AUTH_TOKEN, CONTENT_DEFINITION_ID):
+    if (not AUTH_TOKEN):
+        raise Exception("Authentication Token: The authentication token is invalid")
+  
+    API_URL = f"{ADMIN_API_URL}/content/new?contentDefinitionId={CONTENT_DEFINITION_ID}"
+  
+    # Create header for the request
+    HEADERS = {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer " + AUTH_TOKEN
+    }
+
+    try:
+        RESPONSE = requests.get(API_URL, headers=HEADERS)
+
+        if RESPONSE.ok:
+            ID = json.loads(RESPONSE.text)
+            return ID
+        
+    except:
+        api_exception_handler(RESPONSE, "Get content id failed: ")
+    
