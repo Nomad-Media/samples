53efc30dc0b429bd2dd1005f1b9bf9d833e97773
sfaludi <sfaludi@nomad-cms.com>
2023-07-14 17:14:53 -0700

    fixed string

---
+++ nomad-samples/python/content_api/content/delete_content.py
@@ -0,0 +1,24 @@
+from constants.project_constants import *
+from exceptions.api_exception_handler import *
+
+import requests
+
+def delete_content(AUTH_TOKEN, ID, CONTENT_DEFINITION_ID):
+    if not AUTH_TOKEN:
+        raise Exception("Authorization token not found")
+  
+    API_URL = f"{ADMIN_API_URL}/content/{ID}?contentDefinitionId={CONTENT_DEFINITION_ID}"
+    
+    HEADERS = {
+      "Content-Type": "application/json",
+      "Authorization": "Bearer " + AUTH_TOKEN
+    }
+
+    try:
+        RESPONSE = requests.delete(API_URL, headers=HEADERS)
+
+        if (RESPONSE.ok):
+            return True
+
+    except:
+        api_exception_handler(RESPONSE, "Deleting content failed")
