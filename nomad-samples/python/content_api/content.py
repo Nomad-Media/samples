94e4c15439b78890f18f2db0df8735747d2eda97
sfaludi <sfaludi@nomad-cms.com>
2023-07-17 15:46:10 -0700

    content python

--- nomad-samples/python/content_api/content.py
+++ nomad-samples/python/content_api/content.py
@@ -1,54 +1,36 @@
-from content.get_content import *
-from content.get_contents import *
-from content.create_content import *
-from content.update_content import *
-from content.delete_content import *
+from contents.create_content import *
+from contents.update_content import *
+from contents.delete_content import *
 
-import sys
+import json
 
-def get_contents_main(AUTH_TOKEN):
+def create_content_main(AUTH_TOKEN):
     try:
         CONTENT_DEFINITION_ID = input("Enter contentent definition id: ")
-        SORT_COLUMN = input("Enter sort column (optional): ")
-        SORT_ORDER = input("Enter the order you want to sort the field by (ascending/descending) (optional): ")
-        PAGE_INDEX = input("Enter the page index (optional): ")
-        PAGE_SIZE = input("Enter the page size (optional): ")
-        LANGUAGE_ID = input("Enter the language id (optional): ")
+        
+        print("Creating content")
+        ID = create_content(AUTH_TOKEN, CONTENT_DEFINITION_ID)
+        print(json.dumps(ID, indent=4))
 
-        print("Getting contents")
-        INFO = get_contents(AUTH_TOKEN, CONTENT_DEFINITION_ID, SORT_COLUMN, SORT_ORDER, PAGE_INDEX, PAGE_SIZE, LANGUAGE_ID)
-        print(json.dumps(INFO, indent=4))
     except:
         raise Exception()
 
-def get_content_main(AUTH_TOKEN):
-    try:
-        ID = input("Enter id of the content you want to get: ")
-        CONTENT_DEFINITION_ID = input("Enter contentent definition id: ")
-
-        print("Getting content")
-        INFO = get_content(AUTH_TOKEN, ID, CONTENT_DEFINITION_ID)
-        print(json.dumps(INFO, indent=4))
-    except:
-        raise Exception()
-    
-def update_content_main(AUTH_TOKEN, ID):
+def update_content_main(AUTH_TOKEN):
     try:
         CONTENT_DEFINITION_ID = input("Enter contentent definition id: ")
+        ID = input("Enter the id: ")
 
-        print("Enter/Paste the property json")
-        print("Enter Ctrl + D (Unix) or Ctrl + Z (Windows) to exit")
-
-        PROPERTIES = sys.stdin.read()
+        PROPERTIES = json.loads(input("Enter the property json: \n"))
 
         print("Updating content")
-        update_content(AUTH_TOKEN, ID, CONTENT_DEFINITION_ID, PROPERTIES)
+        INFO = update_content(AUTH_TOKEN, ID, CONTENT_DEFINITION_ID, PROPERTIES)
+        print(json.dumps(INFO, indent=4))
 
     except:
         raise Exception()
 
 def delete_content_main(AUTH_TOKEN):
-    ID = input("Enter id of the content you want to get: ")
+    ID = input("Enter id: ")
     CONTENT_DEFINITION_ID = input("Enter contentent definition id: ")
 
     print("Deleting content")
@@ -56,27 +38,18 @@ def delete_content_main(AUTH_TOKEN):
     print("Content Deleted")
 
 if __name__ == "__main__":
-    AUTH_TOKEN = input("Enter authentication token: ")
+    AUTH_TOKEN = "eyJraWQiOiJkSkpRa3ZxdWxDekpqZEFmWTR0UGwrSytyWldVTE5OTkR1YitYVnljaFNRPSIsImFsZyI6IlJTMjU2In0.eyJzdWIiOiJlYjc1MzI5OC0wODAzLTQyYWEtOTFkMi01NjE3OGE0OTI4NWQiLCJjdXN0b206Y29udGFjdF9pZCI6ImU5YWIxNDFmLWMxMjgtNDE5Yi04YTQ3LWIzNTg1MTQwMzZkNyIsImlzcyI6Imh0dHBzOlwvXC9jb2duaXRvLWlkcC51cy13ZXN0LTIuYW1hem9uYXdzLmNvbVwvdXMtd2VzdC0yX1ZHRXhveTY0aSIsImNvZ25pdG86dXNlcm5hbWUiOiJlYjc1MzI5OC0wODAzLTQyYWEtOTFkMi01NjE3OGE0OTI4NWQiLCJnaXZlbl9uYW1lIjoiU2NvdHQiLCJvcmlnaW5fanRpIjoiNTlkOTdjNGMtNTBjMi00M2NlLWE0OGYtZmFlNDYxMzk5YTBkIiwiYXVkIjoiNWUybm92MXAzYTZxNHM1MHZjamo1ZXNqYjciLCJldmVudF9pZCI6ImY4OWYwMzM4LTQ0YWItNDBiYy1iNTczLTQwNzYzMTYyMDZlZiIsInRva2VuX3VzZSI6ImlkIiwiYXV0aF90aW1lIjoxNjg5NjI5OTIyLCJleHAiOjE2ODk2MzM1MjIsImlhdCI6MTY4OTYyOTkyMiwiZmFtaWx5X25hbWUiOiJGYWx1ZGkiLCJqdGkiOiI5YTA5OTk3Yy0yYTA3LTQzYmMtODA4Mi0zZDMwNDI4NWM3ZGMiLCJlbWFpbCI6InNmYWx1ZGlAbm9tYWQtY21zLmNvbSJ9.LvlQrUdN2JF7IXa5FNpW1TZxlfbl-hkefWBESd0NdElL0gRKqu_AbjK7ibpVlQdg6x80bV8MX5tGDE5Te5UDlNv7VB_7EqPr9iVvfIES21LFKYyhVanyh0QQn2H_hXo9YHkqU46MVxOikbJ_AZGk6fvRIBf_cAzKveuusBy1OKGhTaxux3uuMa9Q0idWGRTppYIlvuAISo_Rgs4TXgCkOt-eT0iau1YBDzN1dvmr3iqJ6GKPw6TgL76EaSlBi1YgJzwyJG1nj_wnsWM66h3DiZQQiKTAnb6n8a1ygp9nGysZLL36rpnabFnKu5A9t-jrzLV_NKBudmk2o83_8lgSoA"
+    print(f"Enter authentication token: {AUTH_TOKEN}")
 
     while True:
-        print("Do you want to get contents, get content by id, create a content, "\
-              "update a content, delete a content, or exit")
-        USER_INPUT = input("Enter get groups, get group, create, update, delete, "\
-                           "or exit for each option above respectivly: ")
-        
-        if USER_INPUT == "get groups":
-            get_contents_main(AUTH_TOKEN)
-        
-        elif USER_INPUT == "get group":
-            get_content_main(AUTH_TOKEN)
+        print("Do you want to create a content, update a content, delete a content, or exit")
+        USER_INPUT = input("Enter create, update, delete, or exit for each option above respectivly: ")
 
-        elif USER_INPUT == "create":
-            ID = create_content(AUTH_TOKEN)
-            update_content_main(AUTH_TOKEN, ID)
+        if USER_INPUT == "create":
+            create_content_main(AUTH_TOKEN)
 
         elif USER_INPUT == "update":
-            ID = input("Enter the id of the movie you want to update: ")
-            update_content_main(AUTH_TOKEN, ID)
+            update_content_main(AUTH_TOKEN)
 
         elif USER_INPUT == "delete":
             delete_content_main(AUTH_TOKEN)
