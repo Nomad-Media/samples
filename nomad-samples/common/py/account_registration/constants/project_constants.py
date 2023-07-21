973328cd3f98a3d1d4cf0e9828778fa0ca154ae4
sfaludi <sfaludi@nomad-cms.com>
2023-07-21 14:24:26 -0700

    changed contstants to remove client domain

--- nomad-samples/common/py/account_registration/constants/project_constants.py
+++ nomad-samples/common/py/account_registration/constants/project_constants.py
@@ -1,4 +1,2 @@
-CLIENT_DOMAIN = "dev-05.demos.media"
-
-PORTAL_API_URL = "https://app-api." + CLIENT_DOMAIN + "/api"
-SERVER_API_URL = "https://admin-app." + CLIENT_DOMAIN + "/api"
+PORTAL_API_URL = "https://app-api.dev-05.demos.media/api"
+ADMIN_API_URL = "https://admin-app.dev-05.demos.media/api"
