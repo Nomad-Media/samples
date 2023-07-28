f71b547ab3fa301e85adfe1c3e92edc26bca7aac
sfaludi <sfaludi@nomad-cms.com>
2023-07-28 15:51:52 -0700

    Changed params

--- nomad-samples/admin/py/basic-live-channel-1.1/live_channel/create_live_channel.py
+++ nomad-samples/admin/py/basic-live-channel-1.1/live_channel/create_live_channel.py
@@ -1,39 +1,35 @@
 from constants.project_constants import *
 from exceptions.api_exception_handler import *
+from helpers.slugify import *
 from live_channel.live_channel_statuses import *
 from live_channel.wait_live_channel_status import *
 from live_channel.live_channel_types import *
 
 import json, requests
 
-def create_live_channel(AUTH_TOKEN, DATA):
-    # Check for valid parameters
-    if (not AUTH_TOKEN or not DATA):
-        raise Exception("Create Live Channel: Invalid API call")
-
-
-    # Build the payload BODY
-    BODY = {
-        "name": DATA["name"],
-        "routeName": DATA["route"],
-        "thumbnailImage": "",
-        "archiveFolderAsset": DATA["archiveFolderAsset"],
-        "isSecureOutput": False,
-        "outputScreenshots": True,
-        "type": { "lookupId": DATA["type"] }
-    }
-
-
-    # Set the appropriate fields based on the channel type
-    if (DATA["type"] == LIVE_CHANNEL_TYPES["External"]):
-        BODY["externalUrl"] = DATA["url"]
-
-
+def create_live_channel(AUTH_TOKEN, NAME, THUMBNAIL_IMAGE, ARCHIVE_FOLDER_ASSET, IS_SECURE_OUTPUT, 
+                        OUTPUT_SCREENSHOTS, TYPE, URL):
     # Create header for the request
     HEADERS = {
         'Content-Type': 'application/json',
         "Authorization": "Bearer " + AUTH_TOKEN
     }
+
+    # Build the payload BODY
+    BODY = {
+        "name": NAME,
+        "routeName": slugify(NAME),
+        "thumbnailImage": THUMBNAIL_IMAGE,
+        "archiveFolderAsset": ARCHIVE_FOLDER_ASSET,
+        "isSecureOutput": IS_SECURE_OUTPUT,
+        "outputScreenshots": OUTPUT_SCREENSHOTS,
+        "type": { "id": TYPE }
+    }
+
+
+    # Set the appropriate fields based on the channel type
+    if (type == LIVE_CHANNEL_TYPES["External"]):
+        BODY["externalUrl"] = URL
 
     try:
         # Send the request
@@ -48,5 +44,5 @@ def create_live_channel(AUTH_TOKEN, DATA):
         return json.loads(RESPONSE.text)
 
     except:
-        api_exception_handler(RESPONSE, f"Created Live Channel {DATA['name']} failed")
+        api_exception_handler(RESPONSE, f"Created Live Channel {NAME} failed")
 
--- nomad-samples/admin/py/basic-live-channel-1.1/live_input/create_live_input.py
+++ nomad-samples/admin/py/basic-live-channel-1.1/live_input/create_live_input.py
@@ -1,17 +1,13 @@
 from constants.project_constants import *
 from exceptions.api_exception_handler import *
+from helpers.slugify import *
 from live_input.live_input_types import *
 from live_input.live_input_statuses import *
 from live_input.wait_live_input_status import *
 
 import json, requests
 
-def create_live_input(AUTH_TOKEN, DATA):
-    # Check for valid parameters
-    if (not AUTH_TOKEN or not DATA):
-        raise Exception("Create Live Input: Invalid API call")
-
-
+def create_live_input(AUTH_TOKEN, NAME, TYPE, SOURCE):
     # Create header for the request
     HEADERS = {
         'Content-Type': 'application/json',
@@ -21,24 +17,24 @@ def create_live_input(AUTH_TOKEN, DATA):
     # Build the payload BODY
     # Error Object of type set is not JSON serializable
     BODY = {
-        "name": DATA["name"],
-        "internalName": DATA["internalName"],
-        "type": { "id": DATA["type"] }
+        "name": NAME,
+        "internalName": slugify(NAME),
+        "type": { "id": TYPE }
     }
 
 
     # Set the appropriate fields based on the type
-    if (DATA["type"] == LIVE_INPUT_TYPES["RTM_PUSH"]):
+    if (type == LIVE_INPUT_TYPES["RTM_PUSH"]):
         pass
-    elif (DATA["type"] == LIVE_INPUT_TYPES["RTMP_PUSH"]):
-        BODY["sourceCidr"] = DATA["source"]
-    elif (DATA["type"] == LIVE_INPUT_TYPES["RTMP_PULL"]):
+    elif (type == LIVE_INPUT_TYPES["RTMP_PUSH"]):
+        BODY["sourceCidr"] = SOURCE
+    elif (type == LIVE_INPUT_TYPES["RTMP_PULL"]):
         pass
-    elif (DATA["type"] == LIVE_INPUT_TYPES["URL_PULL"]):
+    elif (type == LIVE_INPUT_TYPES["URL_PULL"]):
         BODY["sources"] = []
-        BODY["sources"].append({ "url": DATA["source"] })
+        BODY["sources"].append({ "url": SOURCE })
     else:
-        print("Create Live Input: Unknown Live Input Type " + DATA["type"])
+        print("Create Live Input: Unknown Live Input Type " + type)
         exit
 
     try:
@@ -53,5 +49,5 @@ def create_live_input(AUTH_TOKEN, DATA):
         return json.loads(INFO)
     
     except:
-        api_exception_handler(RESPONSE, f"Creating Live Input {DATA['name']} failed")
+        api_exception_handler(RESPONSE, f"Creating Live Input {NAME} failed")
 
