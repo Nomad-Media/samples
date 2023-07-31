352fe5c0caf424f42a7824f906f808c922d68697
sfaludi <sfaludi@nomad-cms.com>
2023-07-30 19:15:39 -0700

    Fixed code

--- nomad-samples/admin/py/basic-live-channel-1.1/live_channel/create_live_channel.py
+++ nomad-samples/admin/py/basic-live-channel-1.1/live_channel/create_live_channel.py
@@ -7,7 +7,8 @@ from live_channel.live_channel_types import *
 
 import json, requests
 
-def create_live_channel(AUTH_TOKEN, NAME, THUMBNAIL_IMAGE, ARCHIVE_FOLDER_ASSET, IS_SECURE_OUTPUT, 
+def create_live_channel(AUTH_TOKEN, NAME, THUMBNAIL_IMAGE, ARCHIVE_FOLDER_ASSET, 
+                        ENABLE_HIGH_AVAILABILITY, ENABLE_LIVE_CLIPPING, IS_SECURE_OUTPUT, 
                         OUTPUT_SCREENSHOTS, TYPE, URL):
     # Create header for the request
     HEADERS = {
@@ -21,25 +22,27 @@ def create_live_channel(AUTH_TOKEN, NAME, THUMBNAIL_IMAGE, ARCHIVE_FOLDER_ASSET,
         "routeName": slugify(NAME),
         "thumbnailImage": THUMBNAIL_IMAGE,
         "archiveFolderAsset": ARCHIVE_FOLDER_ASSET,
+        "enableHighAvailability": ENABLE_HIGH_AVAILABILITY,
+        "enableLiveClipping": ENABLE_LIVE_CLIPPING,
         "isSecureOutput": IS_SECURE_OUTPUT,
         "outputScreenshots": OUTPUT_SCREENSHOTS,
-        "type": { "id": TYPE }
+        "type": { "id": LIVE_CHANNEL_TYPES[TYPE] }
     }
 
 
     # Set the appropriate fields based on the channel type
-    if (type == LIVE_CHANNEL_TYPES["External"]):
+    if (TYPE == LIVE_CHANNEL_TYPES["External"]):
         BODY["externalUrl"] = URL
 
     try:
         # Send the request
         RESPONSE = requests.post(f"{ADMIN_URL}/liveChannel",  headers= HEADERS, data= json.dumps(BODY))
         INFO = json.loads(RESPONSE.text)
-            
-        wait_for_live_channel_status(AUTH_TOKEN, INFO["id"], LIVE_CHANNEL_STATUSES["Idle"], 120, 2)
 
         if not RESPONSE.ok:
             raise Exception()
+            
+        wait_for_live_channel_status(AUTH_TOKEN, INFO["id"], LIVE_CHANNEL_STATUSES["Idle"], 120, 2)
         
         return json.loads(RESPONSE.text)
 
