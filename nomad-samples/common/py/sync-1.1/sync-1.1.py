e237a06e70433ed07d951ec50c9a9c5d956752f0
sfaludi <sfaludi@nomad-cms.com>
2023-08-08 20:20:16 -0700

    added error handling

--- nomad-samples/common/py/sync-1.1/sync-1.1.py
+++ nomad-samples/common/py/sync-1.1/sync-1.1.py
@@ -11,45 +11,55 @@ import json, os
 def sync(AUTH_TOKEN):
 
     print("Syncronizing...")
+    try:
+        # Get movies from json
+        with open("movie.json", "r") as movies_file:
+            MOVIES = json.load(movies_file)
 
-    # Get movies from json
-    with open("movie.json", "r") as movies_file:
-        MOVIES = json.load(movies_file)
+        if (MOVIES is None or len(MOVIES) == 0):
+            raise Exception("Error retrieving data, make sure the source file exists and is not empty.")
 
-    if (MOVIES is None or len(MOVIES) == 0):
-        raise Exception("Error retrieving data, make sure the source file exists and is not empty.")
-        
-    print(f"{len(MOVIES)} movies found in source file")
+        print(f"{len(MOVIES)} movies found in source file")
 
-    # Loop all the movies to sync
-    for idx, JSON_MOVIE in enumerate(MOVIES):
-        print(f"Syncing movie #{idx + 1} out of {len(MOVIES)} | {JSON_MOVIE['title']}")
+        # Loop all the movies to sync
+        for idx, JSON_MOVIE in enumerate(MOVIES):
+            print(f"Syncing movie #{idx + 1} out of {len(MOVIES)} | {JSON_MOVIE['title']}")
 
-        # Check if genre exists if not create it
-        GENRE = check_genres(AUTH_TOKEN, JSON_MOVIE["genre"])
+            # Check if genre exists if not create it
+            GENRE = check_genres(AUTH_TOKEN, JSON_MOVIE["genre"])
 
-        # Check if movie exists
-        MOVIE = get_movie(AUTH_TOKEN, JSON_MOVIE["title"])
+            # Check if movie exists
+            MOVIE = get_movie(AUTH_TOKEN, JSON_MOVIE["title"])
 
-        if (MOVIE["hasItems"]):
-            if sorted(MOVIE.items()) != sorted(JSON_MOVIE.items()):
-                print(f"Updating movie {JSON_MOVIE['title']}")
-                MOVIE = update_movie(AUTH_TOKEN, MOVIE["items"][0]["id"], JSON_MOVIE["id"],
-                                     JSON_MOVIE["title"], JSON_MOVIE["plot"], 
-                                     JSON_MOVIE["releaseDate"], GENRE["id"], GENRE["title"], 
+            if (MOVIE["hasItems"]):
+                if sorted(MOVIE.items()) != sorted(JSON_MOVIE.items()):
+                    print(f"Updating movie {JSON_MOVIE['title']}")
+                    MOVIE = update_movie(AUTH_TOKEN, MOVIE["items"][0]["id"], JSON_MOVIE["id"],
+                                         JSON_MOVIE["title"], JSON_MOVIE["plot"], 
+                                         JSON_MOVIE["releaseDate"], GENRE["id"], GENRE["title"], 
+                                         JSON_MOVIE["image"], JSON_MOVIE["movieFile"])
+            else:
+                print(f"Creating movie {JSON_MOVIE['title']}")
+                MOVIE_ID = create_movie(AUTH_TOKEN)
+                MOVIE = update_movie(AUTH_TOKEN, MOVIE_ID["masterId"], JSON_MOVIE["id"],
+                                     JSON_MOVIE["title"], JSON_MOVIE["plot"],
+                                     JSON_MOVIE["releaseDate"], GENRE["id"], GENRE["title"],
                                      JSON_MOVIE["image"], JSON_MOVIE["movieFile"])
-        else:
-            print(f"Creating movie {JSON_MOVIE['title']}")
-            MOVIE_ID = create_movie(AUTH_TOKEN)
-            MOVIE = update_movie(AUTH_TOKEN, MOVIE_ID["masterId"], JSON_MOVIE["id"],
-                                 JSON_MOVIE["title"], JSON_MOVIE["plot"],
-                                 JSON_MOVIE["releaseDate"], GENRE["id"], GENRE["title"],
-                                 JSON_MOVIE["image"], JSON_MOVIE["movieFile"])
 
-        
-            
-    print("Syncronization completed")
 
+
+        print("Syncronization completed")
+    except:
+        raise Exception("Error syncing movies")
+
+def delete_movie_main(AUTH_TOKEN):
+    INPUT = input("Enter the movie id to delete: ")
+
+    try:
+        delete_movie(AUTH_TOKEN, INPUT)
+        print("Movie deleted")
+    except:
+        raise Exception("Error deleting movie")
 
 if __name__ == "__main__":
     AUTH_TOKEN = input("Enter your auth token: ")
@@ -59,7 +69,7 @@ if __name__ == "__main__":
         if USER_INPUT == "s":    
             sync(AUTH_TOKEN)
         elif USER_INPUT == "d":
-            delete_movie(AUTH_TOKEN, input("Enter the movie id to delete: "))
+            delete_movie_main(AUTH_TOKEN)
         elif USER_INPUT == "exit":
             break
         else:
