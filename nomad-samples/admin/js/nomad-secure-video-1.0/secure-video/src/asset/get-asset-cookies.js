97804a0f4a3a178fc8d7ecbb19de343b13785c8f
sfaludi <sfaludi@nomad-cms.com>
2023-08-08 18:41:56 -0700

    secure video js

---
+++ nomad-samples/admin/js/nomad-secure-video-1.0/secure-video/src/asset/get-asset-cookies.js
@@ -0,0 +1,29 @@
+import * as prjConstants from "../constants/project-constants.js";
+
+/**
+ * Get Related Video Cookies
+ *
+ */
+export default async function getRelatedVideoCookies(assetId, relatedAssetId, authToken) {
+    // Create header for the request
+    const headers = new Headers();
+    headers.append("Authorization", `Bearer ${authToken}`);
+
+    // Send the request
+    const response = await fetch(`${prjConstants.SERVER_URL}/admin/asset/${assetId}/set-cookies/${relatedAssetId}`, {
+        method: "GET",
+        headers: headers,
+    });
+
+    // Check for success
+    if (response.ok) {
+        // Get the response
+        const cookies = await response.json();
+
+        // Return the asset detail object
+        return cookies;
+    }
+
+    // Log error to the console
+    console.log(`Get Related Video Cookies failed: ${response.statusText}`);
+}
