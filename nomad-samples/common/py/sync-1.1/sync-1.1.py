2e0f10a20218aad5bfbe93318c7cd4bebc6e0839
sfaludi <sfaludi@nomad-cms.com>
2023-08-20 15:27:07 -0700

    Added multiple genre selection

--- nomad-samples/common/py/sync-1.1/sync-1.1.py
+++ nomad-samples/common/py/sync-1.1/sync-1.1.py
@@ -15,17 +15,22 @@ def create_movie_main(AUTH_TOKEN):
         PLOT = input("Enter plot: ")
         RELEASE_DATE = input("Enter release date (YYYY-MM-DDTHH:MM:SS): ")
 
-        GENRES = get_genres(AUTH_TOKEN)
+        NOMAD_GENRES = get_genres(AUTH_TOKEN)
         
         GENRE_TITLES = []
-        for GENRE_DICT in GENRES["items"]: GENRE_TITLES.append(GENRE_DICT["title"])
-        GENRE = check_genres(AUTH_TOKEN, input(f"Enter genre: \nGenres: {GENRE_TITLES}\n"))
+        for GENRE_DICT in NOMAD_GENRES["items"]: GENRE_TITLES.append(GENRE_DICT["title"])
+        GENRE_INPUT = input(f"Enter genres (separated by comma): \nGenres: {GENRE_TITLES}\n").split(",")
+        if GENRE_INPUT[0] != "":
+            GENRES = check_genres(AUTH_TOKEN, GENRE_INPUT)
+        else:
+            GENRES = ""
+        
         IMAGE_ID = input("Enter image asset id: ")
         VIDEO_ID = input("Enter video asset id: ")
 
         print("Creating movie...")
         ID = create_movie(AUTH_TOKEN)["contentId"]
-        update_movie(AUTH_TOKEN, ID, ID, TITLE, PLOT, RELEASE_DATE, GENRE["id"], GENRE["title"],
+        update_movie(AUTH_TOKEN, ID, ID, TITLE, PLOT, RELEASE_DATE, GENRES,
                      IMAGE_ID, VIDEO_ID)
         print(f"Movie id: {ID}")
     except:
@@ -49,7 +54,7 @@ def sync(AUTH_TOKEN):
             print(f"Syncing movie #{idx + 1} out of {len(MOVIES)} | {JSON_MOVIE['title']}")
 
             # Check if genre exists if not create it
-            GENRE = check_genres(AUTH_TOKEN, JSON_MOVIE["genre"])
+            GENRES = check_genres(AUTH_TOKEN, JSON_MOVIE["genres"])
 
             # Check if movie exists
             MOVIE = get_movie(AUTH_TOKEN, JSON_MOVIE["title"])
@@ -59,15 +64,15 @@ def sync(AUTH_TOKEN):
                     print(f"Updating movie {JSON_MOVIE['title']}")
                     MOVIE = update_movie(AUTH_TOKEN, MOVIE["items"][0]["id"], JSON_MOVIE["id"],
                                          JSON_MOVIE["title"], JSON_MOVIE["plot"], 
-                                         JSON_MOVIE["releaseDate"], GENRE["id"], GENRE["title"], 
-                                         JSON_MOVIE["image"], JSON_MOVIE["movieFile"])
+                                         JSON_MOVIE["releaseDate"], GENRES, 
+                                         JSON_MOVIE["image"]["id"], JSON_MOVIE["movieFile"]["id"])
             else:
                 print(f"Creating movie {JSON_MOVIE['title']}")
                 MOVIE_ID = create_movie(AUTH_TOKEN)
                 MOVIE = update_movie(AUTH_TOKEN, MOVIE_ID["masterId"], JSON_MOVIE["id"],
-                                     JSON_MOVIE["title"], JSON_MOVIE["plot"],
-                                     JSON_MOVIE["releaseDate"], GENRE["id"], GENRE["title"],
-                                     JSON_MOVIE["image"], JSON_MOVIE["movieFile"])
+                                     JSON_MOVIE["title"], slugify(JSON_MOVIE["title"]), JSON_MOVIE["plot"],
+                                     JSON_MOVIE["releaseDate"], GENRES,
+                                     JSON_MOVIE["image"]["id"], JSON_MOVIE["movieFile"]["id"])
 
 
 
@@ -84,16 +89,16 @@ def update_movie_main(AUTH_TOKEN):
             raise Exception("Movie not found")
         
         MOVIE = MOVIE_JSON["items"][0]
-        GENRES = get_genres(AUTH_TOKEN)
+        NOMAD_GENRES = get_genres(AUTH_TOKEN)
         
         GENRE_TITLES = []
-        for GENRE_DICT in GENRES["items"]: GENRE_TITLES.append(GENRE_DICT["title"])
+        for GENRE_DICT in NOMAD_GENRES["items"]: GENRE_TITLES.append(GENRE_DICT["title"])
 
         print("Press enter to skip parameters")
         TITLE = input("Enter title: ")
         PLOT = input("Enter plot: ")
         RELEASE_DATE = input("Enter release date (YYYY-MM-DDTHH:MM:SS): ")
-        GENRE = input(f"Enter genre: \nGenres: {GENRE_TITLES}\n")
+        GENRES = input(f"Enter genres (separated by comma): \nGenres: {GENRE_TITLES}\n").split(",")
         IMAGE_ID = input("Enter image asset id: ")
         VIDEO_ID = input("Enter video asset id: ")
     
@@ -106,10 +111,10 @@ def update_movie_main(AUTH_TOKEN):
         if RELEASE_DATE == "" and "releaseDate" in MOVIE:
             RELEASE_DATE = MOVIE["releaseDate"]
 
-        if GENRE == "":
-            GENRE_INFO = MOVIE["identifiers"]["genre"]
+        if GENRES[0] == "":
+            GENRES_INFO = MOVIE["identifiers"]["genres"]
         else:
-            GENRE_INFO = check_genres(AUTH_TOKEN, GENRE)
+            GENRES_INFO = check_genres(AUTH_TOKEN, GENRES)
 
         if IMAGE_ID == "" and "image" in MOVIE["identifiers"]:
             IMAGE_ID = MOVIE["identifiers"]["image"]["id"]
@@ -118,7 +123,7 @@ def update_movie_main(AUTH_TOKEN):
             VIDEO_ID = MOVIE["identifiers"]["movieFile"]["id"]
 
         ID = update_movie(AUTH_TOKEN, MOVIE["masterId"], MOVIE["masterId"], TITLE, 
-                          PLOT, RELEASE_DATE, GENRE_INFO["id"], GENRE_INFO["title"], IMAGE_ID, 
+                          PLOT, RELEASE_DATE, GENRES_INFO, IMAGE_ID, 
                           VIDEO_ID)
         print(f"Movie id: {ID}") 
     except:
