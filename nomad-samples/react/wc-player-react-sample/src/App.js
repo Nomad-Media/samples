a005f2ef495c8d621eb0b41a0d4b7f58031fc3d9
Ilias Iliadis <il85@programmer.net>
2023-08-22 21:45:05 +0300

    Allow setting the wc=player vepConfig from React sample

--- nomad-samples/react/wc-player-react-sample/src/App.js
+++ nomad-samples/react/wc-player-react-sample/src/App.js
@@ -2,11 +2,39 @@ import './App.css';
 
 import React, { Component } from 'react';
 class App extends Component {
+
+  defaultVepConfig = {
+    "application": "Embedded",
+    "customer": "dev-05",
+    "siteName": "Dev-05",
+    "hideSidebar": true,
+    "supportSharing": true,
+    "googleTagManager": "GTM-5K22WWM",
+    "players": [{ "format": "progressive", "player": "bitmovin" } ],
+    "applicationId": "fc53821f-43a4-4758-9a31-87e3c66883ef",
+    "bitmovinLicenseKey": "f507de7a-720a-4b61-9698-94dc4a014479",
+    "liveMode": false,
+    "vepModules": [
+      {
+        "moduleType": "streams",
+        "isEnabled": true,
+        "streams": [
+          {
+            "streamUrl": "https://content.dev-05.demos.media/Content/Public/the-joker.mp4",
+            "title": "Test Video 1",
+            "autoplay": true
+          }
+        ]
+      }
+    ]
+  };
+
   constructor(props) {
     super(props);
     this.state = {
       forceStart: undefined,
-      forcePause: undefined
+      forcePause: undefined,
+      vepConfig: this.defaultVepConfig
     };
 
     // This binding is necessary to make `this` work in the callback
@@ -46,6 +74,15 @@ class App extends Component {
     // Handle the wc-player events here
   }
 
+  onConfigFormSubmit = (event) => {
+    event.preventDefault();
+    const updatedVepConfig = event.target.elements.vepConfig.value
+    console.log('updatedVepConfig', updatedVepConfig);
+    this.setState({
+      vepConfig: JSON.parse(updatedVepConfig)
+    });
+  }
+
   handleRef = component => {
     this.component = component;
   };
@@ -65,13 +102,32 @@ class App extends Component {
       margin: "10px"
     };
     
+    const setConfigBtnStyle = {
+      backgroundColor: "blue",
+      color: "white",
+      margin: "10px",
+      padding: "10px"
+    };
+    
     const headerStyle = {
       height: `${headerHeight}px`
     };
 
-    const wcPlayerStyle = {
+    const wcPlayerContainerStyle = {
       height: `calc(100% - ${headerHeight}px)`,
-      display: 'block'
+      display: 'flex',
+    }
+    
+    const wcPlayerStyle = {
+      width: '75%'
+    }
+    
+    const vepConfigFormStyle = {
+      width: '25%'
+    }
+    
+    const vepConfigTextareaStyle = {
+      width: '98%'
     }
 
     return <div className="App">
@@ -79,35 +135,21 @@ class App extends Component {
         <button style={startBtnStyle} onClick={this.onForceStart}>Play</button>
         <button style={pauseBtnStyle} onClick={this.onForcePause}>Pause</button>
       </div>
+      <div style={wcPlayerContainerStyle}>
         <wc-player style={wcPlayerStyle}
             ref={this.handleRef}
             force-start={this.state.forceStart}
             force-pause={this.state.forcePause}
-            vep-config='{
-              "application": "Embedded",
-                "customer": "dev-05",
-                "siteName": "Dev-05",
-                "hideSidebar": true,
-                "supportSharing": true,
-                "googleTagManager": "GTM-5K22WWM",
-                "players": [{ "format": "progressive", "player": "bitmovin" } ],
-                "applicationId": "fc53821f-43a4-4758-9a31-87e3c66883ef",
-                "bitmovinLicenseKey": "f507de7a-720a-4b61-9698-94dc4a014479",
-                "liveMode": false,
-                "vepModules": [
-                  {
-                    "moduleType": "streams",
-                    "isEnabled": true,
-                    "streams": [
-                      {
-                        "streamUrl": "https://content.dev-05.demos.media/Content/Public/the-joker.mp4",
-                        "title": "Test Video 1",
-                        "autoplay": true
-                      }
-                    ]
-                  }
-                ]
-            }'></wc-player>
+            vep-config={JSON.stringify(this.state.vepConfig)}></wc-player>
+        <form style={vepConfigFormStyle} 
+            onSubmit={this.onConfigFormSubmit}>
+          <textarea style={vepConfigTextareaStyle} 
+              name="vepConfig"
+              rows="40"
+              defaultValue={JSON.stringify(this.state.vepConfig, undefined, 2)}></textarea>
+          <button style={setConfigBtnStyle} type='submit'>Set Config</button>
+        </form>
+      </div>
       </div>
     }
 }
