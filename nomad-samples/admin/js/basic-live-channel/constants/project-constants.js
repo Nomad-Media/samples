5795db9e5e602892a643b090e0c245953d020139
scott <slfaludi@gmail.com>
2023-10-23 15:26:19 -0700

    converted basic live channel sample to use sdk

--- nomad-samples/admin/js/basic-live-channel/constants/project-constants.js
+++
@@ -1,6 +0,0 @@
-export const PORTAL_API_URL = `https://app-api.dev-05.demos.media/api`;
-export const ADMIN_API_URL = `https://admin-app.dev-05.demos.media/api`;
-
-export const SLATE_ASSET_ID = "edd704e8-d451-437f-b5ff-2102348f353c";
-export const PUBLIC_FOLDER_ID = "564c5a08-74dd-4197-b1ef-3453a929c9f8";
-export const ARCHIVE_FOLDER_ASSET_ID = "61caabef-e991-49d2-96ce-cda824dcf0bc";
