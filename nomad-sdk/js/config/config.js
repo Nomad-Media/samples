a9c5c5e753b8eacd064e9e9ee88f8bf10414d687
scott <slfaludi@gmail.com>
2023-10-31 13:50:14 -0700

    added config file

---
+++ nomad-sdk/js/config/config.js
@@ -0,0 +1,9 @@
+const config = {
+    "username": "USERNAME",
+    "password": "PASSWORD",
+    "serviceApiUrl": "SERVICE_API_URL",
+    "apiType": "API_TYPE",
+    "debugMode": "DEBUG_MODE",
+};
+
+export default config;
