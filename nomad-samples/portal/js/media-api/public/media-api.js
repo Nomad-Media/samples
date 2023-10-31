64afdb749ccbfb317b9a7d539f1a24decda09cfe
scott <slfaludi@gmail.com>
2023-10-30 19:40:22 -0700

    converted media api to use nomad sdk

---
+++ nomad-samples/portal/js/media-api/public/media-api.js
@@ -0,0 +1,154 @@
+const MEDIA_SEARCH_FORM = document.getElementById("mediaSearchForm");
+const FORM_FORM = document.getElementById("formForm");
+
+const SORT_FIELDS_DIV = document.getElementById("sortFieldsDiv");
+
+const ADD_SORT_FIELDS_BUTTON = document.getElementById("addSortFieldsButton");
+
+ADD_SORT_FIELDS_BUTTON.addEventListener('click', function(event)
+{
+    event.preventDefault();
+
+    let fieldNameLabel = document.createElement('label');
+    fieldNameLabel.setAttribute("for", "sortFieldName");
+    fieldNameLabel.textContent = "Field Name:";
+
+    let fieldName = document.createElement("input");
+    fieldName.setAttribute("type", "text");
+    fieldName.setAttribute("name", "sortFieldName");
+    fieldName.required = true;
+
+    let sortTypeLabel = document.createElement('label');
+    sortTypeLabel.setAttribute("for", "sortType");
+    sortTypeLabel.textContent = "Sort Type:";
+
+    let sortType = document.createElement("select");
+    sortType.setAttribute("name", "sortType");
+    sortType.required = true;
+
+    let ascendingOption = document.createElement("option");
+    ascendingOption.value = "Ascending";
+    ascendingOption.text = "Ascending";
+    sortType.appendChild(ascendingOption);
+
+    let descendingOption = document.createElement("option");
+    descendingOption.value = "Descending";
+    descendingOption.text = "Descending";
+    sortType.appendChild(descendingOption);
+
+    let br1 = document.createElement("br");
+    let br2 = document.createElement("br");
+
+    let removeButton = document.createElement("button");
+    removeButton.textContent = "Remove Sort Field";
+    removeButton.addEventListener("click", function(event) {
+        event.preventDefault();
+        SORT_FIELDS_DIV.removeChild(br1);
+        SORT_FIELDS_DIV.removeChild(br2);
+        SORT_FIELDS_DIV.removeChild(fieldNameLabel);
+        SORT_FIELDS_DIV.removeChild(fieldName);
+        SORT_FIELDS_DIV.removeChild(sortTypeLabel);
+        SORT_FIELDS_DIV.removeChild(sortType);
+        SORT_FIELDS_DIV.removeChild(removeButton);
+    });
+
+    SORT_FIELDS_DIV.appendChild(fieldNameLabel);
+    SORT_FIELDS_DIV.appendChild(fieldName);
+    SORT_FIELDS_DIV.appendChild(sortTypeLabel);
+    SORT_FIELDS_DIV.appendChild(sortType);
+
+    SORT_FIELDS_DIV.appendChild(br1);
+    SORT_FIELDS_DIV.appendChild(br2);
+
+    SORT_FIELDS_DIV.appendChild(removeButton);
+});
+
+MEDIA_SEARCH_FORM.addEventListener("submit", async function (event)
+{
+    event.preventDefault();
+
+    const FORM_DATA = getElements(MEDIA_SEARCH_FORM);
+
+    await sendRequest("/search", "POST", FORM_DATA);
+});
+
+FORM_FORM.addEventListener("submit", async function (event)
+{
+    event.preventDefault();
+
+    const FORM_DATA = getElements(FORM_FORM);
+
+    await sendRequest("/form", "POST", FORM_DATA);
+});
+
+function getElements(FORM)
+{
+    const FORM_DATA = new FormData();
+    for (let input of FORM)
+    {
+        if (input.tagName === "SELECT") {
+            const SELECTED_OPTIONS = []
+            for (let element of input) {
+                if (element.selected) {
+                    if (element.value === element.label) {
+                        if (input.id) {
+                            FORM_DATA.append(input.id, element.value);
+                        } else {
+                            FORM_DATA.append(input.name, element.value);
+                        }
+                    } else {
+                        SELECTED_OPTIONS.push({ id: element.value, description: element.label });
+                    }
+                }
+            }
+            if (SELECTED_OPTIONS.length > 1)
+            {
+                FORM_DATA.append(input.id, JSON.stringify(SELECTED_OPTIONS));
+            }
+            else if (SELECTED_OPTIONS.length === 1)
+            {
+                FORM_DATA.append(input.id, JSON.stringify(SELECTED_OPTIONS[0]));
+            }
+        }
+        else if (input.tagName === "INPUT")
+        {
+            if (input.type === "file") {
+                FORM_DATA.append(input.id, input.files[0]);
+            } else {
+                if (input.id) {
+                    FORM_DATA.append(input.id, input.value);
+                } else {
+                    FORM_DATA.append(input.name, input.value);
+                }
+            }
+        }
+    }
+    return FORM_DATA;
+}
+
+async function sendRequest(PATH, METHOD, BODY)
+{
+    try
+    {
+        const REQUEST = { method: METHOD };
+        if (BODY) REQUEST["body"] = BODY;
+        const RESPONSE = await fetch(PATH, REQUEST);
+
+        if (RESPONSE.ok)
+        {
+            const DATA = await RESPONSE.json();
+            console.log(DATA);
+            return DATA;
+        }
+        else
+        {
+            const INFO = await RESPONSE.json();
+            console.error(JSON.stringify(INFO, null, 4));
+            console.error("HTTP-Error: " + RESPONSE.status);
+        }
+    }
+    catch (error)
+    {
+        console.error(error);
+    }
+}
---
+++ nomad-samples/portal/js/media-api/server.js
@@ -0,0 +1,82 @@
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+import NomadSDK from "../../../../nomad-sdk/js/sdk-debug.js";
+import express from 'express';
+import multer from 'multer';
+
+
+const app = express();
+const upload = multer();
+const port = 4200;
+
+app.use(express.json());
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+	res.sendFile(__dirname + '/public/media-api.html');
+});
+
+app.post('/search', upload.none(), async (req, res) =>
+{
+    try
+    {
+        const IDS = req.body.ids === "" ? [] : req.body.ids.split(',');
+
+        const SORT_FIELDS = [];
+        if (req.body.sortFieldName)
+        {
+            if (typeof req.body.sortFieldName === 'string') 
+            {
+                SORT_FIELDS.push({
+                    fieldName: req.body.sortFieldName,
+                    sortType: req.body.sortType,
+                });
+            }
+            else
+            {
+                for (let idx = 0; idx < req.body.sortFieldName.length; ++idx)
+                {
+                    SORT_FIELDS.push({
+                        fieldName: req.body.sortFieldName[idx],
+                        sortType: req.body.sortType[idx],
+                    });
+                }
+            }
+        }
+
+        const SEARCH_INFO = await NomadSDK.mediaSearch(req.body.searchQuery, 
+            IDS, SORT_FIELDS, req.body.pageOffset, req.body.pageSize);
+
+        res.status(200).json(SEARCH_INFO);
+    }
+    catch (error)
+    {
+        console.error(error);
+        res.status(500).send(error);
+    }
+});
+
+app.post('/form', upload.none(), async (req, res) =>
+{
+    try
+    {
+        const FORM = await NomadSDK.getForm(req.body.id, req.body.firstName, 
+            req.body.lastName, req.body.active, req.body.startDate, req.body.lookupId,
+            req.body.description);
+
+        res.status(200).json(FORM);
+    }
+    catch (error)
+    {
+        console.error(error);
+        res.status(500).send(error);
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
