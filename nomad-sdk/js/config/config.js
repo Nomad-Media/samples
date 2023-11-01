2b0450a0c9dfbfb8274b13de3118da2b79b0c4b5
Scott Faludi <sfaludi@gmail.com>
2023-10-31 17:23:10 -0700

    updated code

--- nomad-sdk/js/config/config.js
+++ nomad-sdk/js/config/config.js
@@ -1,6 +1,6 @@
 const config = {
     "username": "sfaludi@nomad-cms.com",
-    "password": "***REMOVED***!!",
+    "password": "NewJ0bN0madPassword!!",
     "serviceApiUrl": "https://admin-app.dev-05.demos.media/api",
     //"serviceApiUrl": "https://app-api.dev-05.demos.media/api",
     "apiType": "admin",
