43dca6cbf12a025b28ae1b01df99179028a31bf6
Ilias <ilias.livad@gmail.com>
2023-12-06 21:52:35 +0200

    Latest wc-player changes

--- nomad-samples/embedded-player/react/README.md
+++ nomad-samples/embedded-player/react/README.md
@@ -22,20 +22,21 @@
 3. In order to play/pause from external button controls you have to set the `force-start` and `force-pause` input properties of the wc-player. 
 Then you have to bind the values to a button handler. There is a working example in App.js. You can also see in the example below how the two inputs are bound.
 4. In order to enable sharing you have to set the `supportSharing` property of the `vep-config` to `true`
-5. You can control the share icon position by setting `"bottomShareIcon": true`. In this case the icon will be displayed at the bottom.
-6. Also you can control the sharing url by setting `"shareUrl": "<url>"` 
+5. You can control the share icon position by setting `"bottomShareIcon": true`. In this case the icon will be displayed at the bottom. If this value is set to 'false' the share icon will be displayed at the top.
+6. Also you can control the sharing url by setting `"shareUrl": "<url>"`. This can be the URL to the Nomad API endpoint that serves up the HTML and metadata needed for social sharing of the asset. Alternatively this can be the URL of the web page the embedded player has been included in.
 7. You can also specify which media options you support, setting these `vep-config` properties to `true`: 
 `supportSharingFacebook`, `supportSharingTwitter`, `supportSharingLinkedin`, `supportSharingEmail`. If not specified, this defaults to showing all media.
 In the example below we support all media except for twitter which is set to `false`.
 8. You can disable 'seek' gtm event tracking by setting `"skipGtmSeek": true` in the vepConfig.
 9. We can set the quad-view setting `"showTileMode": true` in the config and having specified multiple streams. Currently we display up to 4 videos at the same time
-10. The share menu panel UI can be changed using these CSS variables, in the public/styles.css:
+10. In order to use the plain video player without the Pin and Expand controls, you have to set `"showTileMode": false`
+11. The share menu panel UI can be changed using these CSS variables, in the public/styles.css:
 --custom-menu-button-icon--color
 --form--submit-button-bg-color
 --form--submit-button-txt-color
 --form--field-padding
 --form--button-padding
-11. There is an option to enable debug mode and see some internal logs about the player. Just add the `debug-mode` attribute to the `wc-player`.
+12. There is an option to enable debug mode and see some internal logs about the player. Just add the `debug-mode` attribute to the `wc-player`.
 
 There is a `playerEventsChanges` event thrown by the wc-player that has a certain type. Here is the current event type list: `sourceloaded`, `play`, `pause` and `ended`.
 You can refer to some example screenshots in the `screenshots` folder.
@@ -70,6 +71,8 @@ Example usage of wc-player:
               "siteName": "Acme Main Site",
               "hideSidebar": true,
               "supportSharing": true,
+	      "shareUrl": "Nomad sharing HTML endpoint URL or website page URL goes here",
+              "bottomShareIcon": true,
               "supportSharingFacebook": true,
               "supportSharingTwitter": false,
               "supportSharingLinkedin": true,
--- nomad-samples/embedded-player/react/src/App.js
+++ nomad-samples/embedded-player/react/src/App.js
@@ -5,35 +5,87 @@ class App extends Component {
 
   defaultVepConfig = {
     "application": "Embedded",
-    "customer": "Test Customer",
-    "siteName": "Test Site",
-    "hideSidebar": true,
-    "supportSharing": false,
+    "customer": "dev-05",
+    "siteName": "Dev-05",
+    "hideSidebar": false,
+    "supportSharing": true,
     "bottomShareIcon": true,
-    "googleTagManager": "GTM-0000000",
+    "shareUrl": "https://app-api.dev-05.demos.media/api/embedded/share-media",
+    "googleTagManager": "GTM-5K22WWM",
     "players": [
-      {
-        "format": "hls",
-        "player": "bitmovin"
-      },
       {
         "format": "progressive",
         "player": "bitmovin"
       }
     ],
-    "bitmovinLicenseKey": "000000000000000000000000000000000000000",
+    "applicationId": "fc53821f-43a4-4758-9a31-87e3c66883ef",
+    "bitmovinLicenseKey": "f507de7a-720a-4b61-9698-94dc4a014479",
     "liveMode": false,
     "vepModules": [
       {
         "moduleType": "streams",
         "isEnabled": true,
-        "showTileMode": false,
+        "showTileMode": true,
         "streams": [
           {
             "id": "1",
             "streamUrl": "https://content.dev-05.demos.media/Content/Public/the-joker.mp4",
             "title": "Test Video 1",
             "autoplay": true
+          },
+          {
+            "id": "2",
+            "streamUrl": "https://content.dev-05.demos.media/Content/Public/the-batman.mp4",
+            "title": "Test Video 2 Test Video 2 Test Video 2 Test Video 2 Test Video 2 Test Video 2 Test Video 2 \n            Test Video 2 Test Video 2 Test Video 2 Test Video 2 Test Video 2 Test Video 2 Test Video 2 Test Video 2 \n            Test Video 2 Test Video 2 Test Video 2 Test Video 2 Test Video 2 Test Video 2 Test Video 2 Test Video 2!!!",
+            "autoplay": false
+          },
+          {
+            "id": "3",
+            "streamUrl": "https://content.dev-05.demos.media/Content/Public/the-joker.mp4",
+            "title": "Test Video 3",
+            "autoplay": true
+          },
+          {
+            "id": "4",
+            "streamUrl": "https://content.dev-05.demos.media/Content/Public/the-batman.mp4",
+            "title": "Test Video 4",
+            "autoplay": false
+          },
+          {
+            "id": "5",
+            "streamUrl": "https://content.dev-05.demos.media/Content/Public/the-joker.mp4",
+            "title": "Test Video 5",
+            "autoplay": true
+          },
+          {
+            "id": "6",
+            "streamUrl": "https://content.dev-05.demos.media/Content/Public/the-batman.mp4",
+            "title": "Test Video 6",
+            "autoplay": false
+          },
+          {
+            "id": "7",
+            "streamUrl": "https://content.dev-05.demos.media/Content/Public/the-joker.mp4",
+            "title": "Test Video 7",
+            "autoplay": true
+          },
+          {
+            "id": "8",
+            "streamUrl": "https://content.dev-05.demos.media/Content/Public/the-batman.mp4",
+            "title": "Test Video 8",
+            "autoplay": false
+          },
+          {
+            "id": "9",
+            "streamUrl": "https://content.dev-05.demos.media/Content/Public/the-joker.mp4",
+            "title": "Test Video 9",
+            "autoplay": true
+          },
+          {
+            "id": "10",
+            "streamUrl": "https://content.dev-05.demos.media/Content/Public/the-batman.mp4",
+            "title": "Test Video 10",
+            "autoplay": false
           }
         ]
       }
