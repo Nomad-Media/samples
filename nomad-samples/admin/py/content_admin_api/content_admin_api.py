f15b59faea75090c80165e2b5ef55115e6b3ff31
scott <slfaludi@gmail.com>
2023-12-27 22:54:11 -0800

    changed sample to use pip

--- nomad-samples/admin/py/content_admin_api/content_admin_api.py
+++ nomad-samples/admin/py/content_admin_api/content_admin_api.py
@@ -1,13 +1,14 @@
-from content.add_custom_properties import *
-from content.add_related_content import *
-from content.add_tag_or_collection import *
-from content.delete_related_content import *
-from content.delete_tag_or_collection import *
-from content.remove_tag_or_collection import *
+import sys, os
+sys.path.append(os.path.realpath('...'))
+
+from nomad_media_pip.nomad_sdk import Nomad_SDK
+from config import config
+
+nomad_sdk = Nomad_SDK(config)
 
 import json
 
-def add_tag_or_collection_main(AUTH_TOKEN):
+def add_tag_or_collection_main():
     try:
         TYPE = "tag" if input("Enter tag or collection for what type of content you want to add: ") == "tag" else "collection"
         CONTENT_ID = input(f"Enter the content id of the {TYPE}: ")
@@ -18,13 +19,13 @@ def add_tag_or_collection_main(AUTH_TOKEN):
         CREATE_NEW_BOOL = True if CREATE_NEW == "true" else False
 
         print(f"Adding {TYPE}")
-        TAG_INFO = add_tag_or_collection(AUTH_TOKEN, TYPE, CONTENT_ID, CONTENT_DEFINITION, NAME, TAG_ID, CREATE_NEW_BOOL)
+        TAG_INFO = nomad_sdk.add_tag_or_collection(TYPE, CONTENT_ID, CONTENT_DEFINITION, NAME, TAG_ID, CREATE_NEW_BOOL)
         print(json.dumps(TAG_INFO, indent=4))
     except:
         raise Exception()
     
 
-def remove_tag_or_collection_main(AUTH_TOKEN):
+def remove_tag_or_collection_main():
     try:
         TYPE = "tag" if input("Enter tag or collection for what type of content you want to remove: ") == "tag" else "collection"
         TAG_ID = input(f"Enter the {TYPE} id: ")
@@ -32,48 +33,48 @@ def remove_tag_or_collection_main(AUTH_TOKEN):
         CONTENT_DEFINITION = input(f"Enter content definition of the {TYPE}: ")
 
         print(f"Removing {TYPE}")
-        REMOVE_TAG_INFO = remove_tag_or_collection(AUTH_TOKEN, TYPE, CONTENT_ID, TAG_ID, CONTENT_DEFINITION)
+        REMOVE_TAG_INFO = nomad_sdk.remove_tag_or_collection(TYPE, CONTENT_ID, TAG_ID, CONTENT_DEFINITION)
         print(json.dumps(REMOVE_TAG_INFO, indent=4))
     except:
         raise Exception()
 
 
-def delete_tag_or_collection_main(AUTH_TOKEN):
+def delete_tag_or_collection_main():
     try:
         TYPE = "tag" if input("Enter tag or collection for what type of content you want to delete: ") == "tag" else "collection"
         TAG_ID = input(f"Enter the {TYPE} id: ")
 
         print(f"Deleting {TYPE}")
-        DELETE_TAG_INFO = delete_tag_or_collection(AUTH_TOKEN, TYPE, TAG_ID)
+        DELETE_TAG_INFO = nomad_sdk.delete_tag_or_collection(TYPE, TAG_ID)
         print(json.dumps(DELETE_TAG_INFO, indent=4))
     except:
         raise Exception()
 
-def add_related_content_main(AUTH_TOKEN):
+def add_related_content_main():
     try:
         CONTENT_ID = input("Enter the content id: ")
         RELATED_CONTENT_ID = input("Enter the content id of the related content: ")
         CONTENT_DEFINITION = input("Enter content definition: ")
 
         print("Creating related content")
-        RELATED_CONTENT_INFO = add_related_content(AUTH_TOKEN, CONTENT_ID, RELATED_CONTENT_ID, CONTENT_DEFINITION)
+        RELATED_CONTENT_INFO = nomad_sdk.add_related_content(CONTENT_ID, RELATED_CONTENT_ID, CONTENT_DEFINITION)
         print(json.dumps(RELATED_CONTENT_INFO, indent=4))
     except:
         raise Exception()
     
-def delete_related_content_main(AUTH_TOKEN):
+def delete_related_content_main():
     try:
         CONTENT_ID = input("Enter the content id: ")
         RELATED_CONTENT_ID = input("Enter the content id of the related content: ")
         CONTENT_DEFINITION = input("Enter content definition: ")
 
         print("Deleting related content")
-        INFO = delete_related_content(AUTH_TOKEN, CONTENT_ID, RELATED_CONTENT_ID, CONTENT_DEFINITION)
+        INFO = nomad_sdk.delete_related_content(CONTENT_ID, RELATED_CONTENT_ID, CONTENT_DEFINITION)
         print(json.dumps(INFO, indent=4))
     except:
         raise Exception()
 
-def add_custom_properties_main(AUTH_TOKEN):
+def add_custom_properties_main():
     try:
         ASSET_ID = input("Enter the asset id of the asset you want to add the custom property to: ")
         DISPLAY_NAME = input("Enter the name of the asset you want to add the custom property to: ")
@@ -87,34 +88,74 @@ def add_custom_properties_main(AUTH_TOKEN):
             if USER_INPUT == "n":
                 break
             
-        INFO = add_custom_properties(AUTH_TOKEN, ASSET_ID, DISPLAY_NAME, CUSTOM_PROPERTIES)
+        INFO = nomad_sdk.add_custom_properties(ASSET_ID, DISPLAY_NAME, CUSTOM_PROPERTIES)
         print(json.dumps(INFO, indent=4))
     except:
         raise Exception()
+    
+def bulk_update_metadata_main():
+    try:
+        CONTENT_IDS = input("Enter the content ids of the content you want to update: (separate by comma) ").replace(" ","").split(",")
+        COLLECTION_IDS = input("Enter the collection ids of the content you want to update: (separate by comma) ").replace(" ","").split(",") if input("Do you want to add collection ids (y/n): ") == "y" else []
+        RELATED_CONTENT_IDS = input("Enter the related content ids of the content you want to update: (separate by comma) ").replace(" ","").split(",") if input("Do you want to add related content ids (y/n): ") == "y" else []
+        TAG_IDS = input("Enter the tag ids of the content you want to update: (separate by comma) ").replace(" ","").split(",") if input("Do you want to add tag ids (y/n): ") == "y" else []
+        SCHEMA_NAME = input("Enter the schema name of the content you want to update: ") if input("Do you want to add schema name (y/n): ") == "y" else ""
+            
+        INFO = nomad_sdk.bulk_update_metadata(CONTENT_IDS, COLLECTION_IDS, RELATED_CONTENT_IDS,
+                                              TAG_IDS, SCHEMA_NAME)
+        print(json.dumps(INFO, indent=4))
+    except:
+        raise Exception()
+    
+def create_tag_or_collection_main():
+    try:
+        TYPE = "tag" if input("Enter tag or collection for what type of content you want to create: ") == "tag" else "collection"
 
+        NAME = input(f"Enter the name of the {TYPE}: ")
+
+        print(f"Creating {TYPE}")
+        TAG_INFO = nomad_sdk.create_tag_or_collection(TYPE, NAME)
+        print(json.dumps(TAG_INFO, indent=4))
+    except:
+        raise Exception()
+    
+def get_tag_or_collection_main():
+    try:
+        TYPE = "tag" if input("Enter tag or collection for what type of content you want to get: ") == "tag" else "collection"
+        TAG_ID = input(f"Enter the {TYPE} id: ")
+
+        print(f"Getting {TYPE}")
+        TAG_INFO = nomad_sdk.get_tag_or_collection(TYPE, TAG_ID)
+        print(json.dumps(TAG_INFO, indent=4))
+    except:
+        raise Exception()
 
 if __name__ == "__main__":
-    AUTH_TOKEN = input("Enter your authentication token: ")
-
     while True:
         print("Do you want to add a tag/collection, delete a tag/collection, remove a tag/collection, "\
               "add related content, delete related content, add custom properties, "\
-              "create a content defintion, update a content definition, or exit")
+              "bulk update metadata, create tag/collecton, get tag/collection, or exit")
         USER_INPUT = input("Enter add tag/collection, remove tag/collection, delete tag/collection, "\
-                           "add related, delete related, add custom, create def, update def, or exit "\
+                           "add related, delete related, add custom, bulk, create, get, or exit "\
                            "for each option above respetively: ")
         
         if USER_INPUT == "add tag/collection":
-            add_tag_or_collection_main(AUTH_TOKEN)
+            add_tag_or_collection_main()
         elif USER_INPUT == "remove tag/collection":
-            remove_tag_or_collection_main(AUTH_TOKEN)
+            remove_tag_or_collection_main()
         elif USER_INPUT == "delete tag/collection":
-            delete_tag_or_collection_main(AUTH_TOKEN)
+            delete_tag_or_collection_main()
         elif USER_INPUT == "add related":
-            add_related_content_main(AUTH_TOKEN)
+            add_related_content_main()
         elif USER_INPUT == "delete related":
-            delete_related_content_main(AUTH_TOKEN)
-        elif USER_INPUT == "add custom":
+            delete_related_content_main()
+        elif USER_INPUT == "bulk":
+            bulk_update_metadata_main()
+        elif USER_INPUT == "create":
+            create_tag_or_collection_main()
+        elif USER_INPUT == "get":
+            get_tag_or_collection_main()
+        elif USER_INPUT == "exit":
             break
         else:
             print("Input incorrect")
