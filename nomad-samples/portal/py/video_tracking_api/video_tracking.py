9361809c46ff33b8d5487ae19cc1eeec70eb90a6
scott <slfaludi@gmail.com>
2024-01-02 12:24:59 -0800

    changed sample to use pip

--- nomad-samples/portal/py/video_tracking_api/video_tracking.py
+++ nomad-samples/portal/py/video_tracking_api/video_tracking.py
@@ -1,29 +1,33 @@
-from track_video.track_video_seconds import *
-from track_video.track_video_hidden import *
-from track_video.track_video_quartile import *
+import sys, os
+sys.path.append(os.path.realpath('...'))
 
-def track_video_seconds_main(AUTH_TOKEN):
+from nomad_media_pip.nomad_sdk import Nomad_SDK
+from config import config
+
+nomad_sdk = Nomad_SDK(config)
+
+def track_video_seconds():
     ASSET_ID = input("Enter the asset id of the video you want to track: ")
     SECONDS = input("How many seconds do you want the video beacon progress tracking data?: ")
 
     print("Tracking video with seconds")
-    track_video_seconds(AUTH_TOKEN, ASSET_ID, SECONDS)
+    nomad_sdk.get_video_tracking(ASSET_ID, 0, SECONDS)
     print("Tracking video success")
 
-def track_video_quartile_main(AUTH_TOKEN):
+def track_video_quartile():
     ASSET_ID = input("Enter the asset id of the video you want to track: ")
     QUARTILE = input("Enter the quartile you want to track: ")
 
     ORDINAL = ["first", "second", "third", "fourth"]
     print(f"Tracking {ORDINAL[int(QUARTILE) - 1]} quartile")
-    track_video_quartile(AUTH_TOKEN, ASSET_ID, QUARTILE)
+    nomad_sdk.get_video_tracking(ASSET_ID, QUARTILE, None)
     print("Tracking video success")
 
-def track_video_hidden_main(AUTH_TOKEN):
+def track_video_hidden():
     ASSET_ID = input("Enter the asset id of the video you want to track: ")
 
     print("Hiding video tracking")
-    track_video_hidden(AUTH_TOKEN, ASSET_ID)
+    nomad_sdk.get_video_tracking(ASSET_ID, 5, None)
     print("Hiding video tracking success")
 
 if __name__ == "__main__":
@@ -33,11 +37,11 @@ if __name__ == "__main__":
         USER_INPUT = input("Enter seconds, quartile, or hidden for each how you want "\
                       "to track your video, or exit to exit: ")
         if USER_INPUT == "seconds":
-            track_video_seconds_main(AUTH_TOKEN)
+            track_video_seconds()
         elif USER_INPUT == "quartile":
-            track_video_quartile_main(AUTH_TOKEN)
+            track_video_quartile()
         elif USER_INPUT == "hidden":
-            track_video_hidden_main(AUTH_TOKEN)
+            track_video_hidden()
         elif USER_INPUT == "exit":
             break
         else:
